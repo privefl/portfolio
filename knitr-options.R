4d66788fdbbeae9a6f909f9afd722f4d02374a4e
Florian Privé <florian.prive.21@gmail.com>
2018-05-28 14:42:12 +0200

    init

---
+++ knitr-options.R
@@ -0,0 +1,10 @@
+knitr::opts_chunk$set(
+  comment = "#>",
+  out.width = "70%",
+  fig.align = 'center',
+  dev = "svg"
+)
+
+options(width = 100)
+
+Sys.setenv(LANGUAGE = "en")
---
+++ spelling-check.R
@@ -0,0 +1,16 @@
+if (!exists("WORDS_TO_IGNORE")) WORDS_TO_IGNORE <- character(0)
+
+# Spelling check
+spelling_errors <- spelling::spell_check_files(
+  path = knitr::current_input(), #list.files(pattern = "\\.Rmd$"),
+  ignore = c(scan(system.file("WORDLIST", package = "advr38pkg"), ""),
+             WORDS_TO_IGNORE),
+  lang = "en-US"
+)
+
+if (nrow(spelling_errors)) {
+  capture.output(print(spelling_errors), file = "spelling.errors")
+  stop("Check spelling! Check file 'spelling.errors'")
+}
+
+rm(spelling_errors)
